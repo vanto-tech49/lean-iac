b915be18bbecb37b775c5cd18b9273d80021d8c5
tech49 <vanto-tech49@users.noreply.github.com>
2025-05-15 13:47:30 +0200

    Try to fix module path resolution.

--- src/terraform/platform/core/main.tf
+++ src/terraform/platform/core/main.tf
@@ -1,5 +1,5 @@
 module "resource-group" {
-  source               = "../../modules/azure-resource-group"
+  source               = "./../../modules/azure-resource-group"
   ressource_group_name = var.ressource_group_name
   location             = var.location
 }
