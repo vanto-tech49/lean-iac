9c6ca68c9a4f578ff871adf0cfb64a5d6809bccd
tech49 <vanto-tech49@users.noreply.github.com>
2025-05-15 16:16:25 +0200

    Decrease azurerm version to try to fix the provider issue.

--- src/terraform/platform/core/providers.tf
+++ src/terraform/platform/core/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.11.0"
+  required_version = "~> 1.11.4"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.28.0"
+      version = "~> 3.117.1"
     }
   }
 
